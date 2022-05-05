18ab5c71d526d695283a2b232cebe3c153f0065f
Doug Wolf <douglaswolf@yahoo.com>
2022-05-04 20:45:39 -0700

    No commit message

--- axi4lite.h
+++ axi4lite.h
@@ -13,7 +13,7 @@ public:
 
     // AXI write transaction - Writes the specified data to the specified address
     // Returns non-zero on error (returns the value from the AXI bresp channel)
-	uint8_t write(const uint32_t addr, const uint32_t data);
+    uint8_t write(const uint32_t addr, const uint32_t data);
     
     // AXI read transaction - Reads the specified address.   Returns non-zero on
     // error. (returns the value of the AXI rresp channel)
@@ -24,6 +24,6 @@ private:
     typedef ap_uint<65> int65;
     typedef ap_uint<34> int34;
 
-	hls::stream< int65 > _cmd_fifo;
-	hls::stream< int34 > _rsp_fifo;
+    hls::stream< int65 > _cmd_fifo;
+    hls::stream< int34 > _rsp_fifo;
 };
